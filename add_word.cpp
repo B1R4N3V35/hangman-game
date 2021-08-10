c5770d753f0d942a876ee5df0bc871299d57dabd
Ubirajara Neves <ubirajara.neves@usp.br>
2021-08-10 21:12:44 +0000

    Checking if a new word is already in the bank

--- add_word.cpp
+++ add_word.cpp
@@ -5,6 +5,7 @@
 #include "read_file.h"
 #include "save_file.h"
 #include "sort.h"
+#include "search.h"
 
 void add_word(std::string &file_name) {
 
@@ -13,11 +14,20 @@ void add_word(std::string &file_name) {
   std::cin >> new_word;
 
   std::vector<std::string> words_list = read_file(file_name);
-  words_list.push_back(new_word);
-  sort(words_list);
 
-  save_file(words_list, file_name);
+  if (search(words_list, new_word) == -1) {
 
-  std::cout << "Your word was added to the bank. Thanks!" << std::endl;
+    words_list.push_back(new_word);
+    sort(words_list);
+
+    save_file(words_list, file_name);
+
+    std::cout << "Your word was added to the bank. Thanks!" << std::endl;
+
+  } else {
+
+    std::cout << "The word " << new_word << " already exists in the bank." << std::endl;
+
+  }
 
 }
